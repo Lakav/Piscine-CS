65bb37ab12c1de4dc342e67859a67f4cc2a47d1a
julescavanier <jules.cavanier@ynov.com>
2022-09-12 09:32:44 +0200

    IsOdd

---
+++ IsOdd.cs
@@ -0,0 +1,12 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class IsOdd_Exercice
+    {
+        public static bool IsOdd(int a)
+        {
+            if ( a % 2 != 0 )
+                return false;
+            return true;
+        }
+    }
+}
