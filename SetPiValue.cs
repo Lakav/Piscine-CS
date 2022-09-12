8543408f52bab16a6207e4ab7c61a38bcf1035ee
julescavanier <jules.cavanier@ynov.com>
2022-09-12 09:45:08 +0200

    pi

---
+++ SetPiValue.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class SetPiValue_Exercice
+    {
+        public static double SetPiValue(double pi)
+        {
+            pi = 3.14159;
+            return pi;
+        }
+    }
+}
