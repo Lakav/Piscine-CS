6b2e923e67f20564b177846a9ddd081b43473010
julescavanier <jules.cavanier@ynov.com>
2022-09-13 07:55:51 +0200

    ok

--- GetStringSize.cs
+++ GetStringSize.cs
@@ -4,10 +4,7 @@ namespace CSharpDiscovery.Quest01
     {
         public static int GetStringSize(string str)
         {
-            if (str.Length < 0)
-            {
-                return 0;
-            }
+
             return str?.Length ?? 0; //return str.Length
 
         }
