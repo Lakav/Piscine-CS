74bf0e6e71d863280e1141c53c6e036d542cbc78
julescavanier <jules.cavanier@ynov.com>
2022-09-14 10:24:11 +0200

    ok

--- DisplayDateWithoutTime.cs
+++ DisplayDateWithoutTime.cs
@@ -6,7 +6,7 @@ namespace CSharpDiscovery.Quest02
     {
         public static string DisplayDateWithoutTime(DateTime date)
         {
-            string strTodayFR = date.ToString("dd/MM/yyyy");
+            string strTodayFR = date.ToString("dd/MM/yyyy HH:mm:ss");
             return strTodayFR;
 
         }
