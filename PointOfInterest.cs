69684a61be968161ded3bf0fc3f274a8221b6042
julescavanier <jules.cavanier@ynov.com>
2022-09-14 14:47:12 +0200

    ok

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -10,7 +10,19 @@ namespace CSharpDiscovery.Quest03
         public string Name { get; set; } = null;
         public static string GoogleMapsUrlTemplate { get; set; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
+        public PointOfInterest()
+        {
+            Name = "Bordeaux Ynov Campus";
+            Latitude = 44.854186;
+            Longitude = -0.5663056;
+        }
 
-
+        public PointOfInterest(string _Name, double _Latitude, double _Longitude)
+        {
+            Name = _Name;
+            Latitude = _Latitude;
+            Longitude = _Longitude;
+        }
+        
     }
 }
