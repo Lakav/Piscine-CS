55069b84fff81848ecf3dd33d4af6cff1053f4a4
julescavanier <jules.cavanier@ynov.com>
2022-09-14 09:14:16 +0200

    ok

---
+++ DisplayDateWithoutTime.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class DisplayDateWithoutTime_Exercice
+    {
+        public static string DisplayDateWithoutTime(DateTime date)
+        {
+            string strTodayFR = date.ToString("dd/MM/yyyy");
+            return strTodayFR;
+
+        }
+    }
+}
