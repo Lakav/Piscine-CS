49e438fd7c000d43602a58e134ac6df153dc2866
julescavanier <jules.cavanier@ynov.com>
2022-09-19 11:54:27 +0200

    ok

---
+++ Car.cs
@@ -0,0 +1,33 @@
+namespace CSharpDiscovery.Quest04
+{
+    public class Car:Vehicule
+    {
+        public string Model { get; set; }
+
+        public Car() : base()
+        {
+            Brand = "Unknown";
+        }
+        
+        public Car(string _Brand, string _Model, string _Color, int _CurrentSpeed = 0)
+            : base(_Brand,_Color,_CurrentSpeed)
+        {
+            Model = _Model;
+        }
+        
+        public override string ToString()
+        {
+            string str ="(Color) (Brand) (Model)";
+            str = str.Replace("(Color)", Color);
+
+            string strB = str;
+            strB = str.Replace("(Brand)", Brand);
+
+            string strM = strB;
+            strM = strB.Replace("(Model)", Model);
+
+            return strM;
+        }
+        
+    }
+}
