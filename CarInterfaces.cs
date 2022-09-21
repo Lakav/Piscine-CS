593f99683706ab4b55aeeed51360cde97dc10225
julescavanier <jules.cavanier@ynov.com>
2022-09-21 09:05:06 +0200

    ok

---
+++ CarInterfaces.cs
@@ -0,0 +1,17 @@
+namespace CSharpDiscovery.Quest04
+{
+    public interface IThermalCar
+    {
+        public int FuelLevel { get; set; }
+        void FillUp();
+        int GetFuelLevel();
+    }
+
+    public interface IElectricCar
+    {
+        public int BatteryLevel { get; set; }
+        void Recharge();
+        int GetBatteryLevel();
+    }
+
+}
